c136e2477cc6ba42d8e427db636e74f10a446fc4
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-11-12 01:34:37 +0200

    Edit admin panel

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth.admin import UserAdmin
+from .models import (
+    Sport,
+    SportsClub,
+    Trainer,
+    Workout,
+)
 
-# Register your models here.
+
+@admin.register(Trainer)
+class TrainerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("sports_club", "sport",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("sports_club", "sport", "experience", "trainer_avatar",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "sports_club",
+                        "sport",
+                        "experience",
+                        "trainer_avatar",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(Workout)
+admin.site.register(SportsClub)
+admin.site.register(Sport)
