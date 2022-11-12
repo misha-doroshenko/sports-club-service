9ebc6259a390aa9d8379f3fb5708f250ddae8a26
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-11-12 13:42:31 +0200

    Commit project files

--- club/apps.py
+++ club/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ClubConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'club'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "club"
