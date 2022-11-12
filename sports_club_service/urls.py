ba6077b9831ed6f895a20b2e315b53010e0ae5a9
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-11-12 20:55:14 +0200

    Install django debug toolbar

--- sports_club_service/urls.py
+++ sports_club_service/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("club.urls", namespace="club"))
+    path("", include("club.urls", namespace="club")),
+    path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
