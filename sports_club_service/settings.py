c132af71e03973c7c4204eb1b0dd9288aee46bf3
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-11-13 18:19:42 +0200

    Add sport update view with template

--- sports_club_service/settings.py
+++ sports_club_service/settings.py
@@ -124,12 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-STATICFILES_DIRS = (BASE_DIR / "static",)
 
 
 # Default primary key field type
