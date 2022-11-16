695c123b5c806abf74f2dc8fab8f25ff201e59ad
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-11-16 05:49:37 +0200

    Update settings.py

--- sports_club_service/settings.py
+++ sports_club_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--c4ga^wjnil*@iz-iqc8ikd_i93wgacg^*8&32#l#x&+h0a(fg"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure--c4ga^wjnil*@iz-iqc8ikd_i93wgacg^*8&32#l#x&+h0a(fg")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
